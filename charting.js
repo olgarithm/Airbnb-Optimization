8e6da6598c97e8b9da9718a1c559f9e76a3eef4a
olgatron9000 <olga.andreeva@hotmail.com>
2017-11-12 20:59:05 -0800

    Bar and pie chart + flexbox

--- charting.js
+++ charting.js
@@ -7,27 +7,98 @@
 
 	function showCharts() {
 		var seeCharts = document.getElementById("seeCharts");
-		var chart = document.getElementById("chart1");
+		var chart = document.getElementById("charts");
+		var childDivs = document.getElementById('charts').getElementsByTagName('div');
 		console.log(seeCharts.innerHTML);
 		if (seeCharts.innerHTML.indexOf("See") > -1) {
-			chart.classList.remove("hidden");
+			for (i = 0; i < childDivs.length; i++ ) {
+				 childDivs[i].classList.remove("hidden");
+			}
 			seeCharts.innerHTML = "Hide my cool charts!"
 		} else {
-			chart.classList.add("hidden");
+			for (i = 0; i < childDivs.length; i++ ) {
+				 childDivs[i].classList.add("hidden");
+			}
 			seeCharts.innerHTML = "See my cool charts!"
 		}
 		
 	}
 
-	function createCharts() {
-		var ctx = document.getElementById("myChart");
+	function createCharts(propertyMap) {
+		var ctx = document.getElementById("firstChart");
 		var myChart = new Chart(ctx, {
 		    type: 'bar',
 		    data: {
-		        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		        labels: ["Apartment", "House", "Condominum", "Bed & Breakfast", "Boat", "Treehouse"],
 		        datasets: [{
-		            label: '# of Votes',
-		            data: [12, 19, 3, 5, 2, 3],
+		            label: '# of Listings',
+		            data: [5002, 2489, 547, 92, 6, 3],
+		            backgroundColor: [
+		                'rgba(255, 99, 132, 1)',
+		                'rgba(54, 162, 235, 1)',
+		                'rgba(255, 206, 86, 1)',
+		                'rgba(75, 192, 192, 1)',
+		                'rgba(153, 102, 255, 1)',
+		                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderColor: [
+		                'rgba(255,99,132,1)',
+		                'rgba(54, 162, 235, 1)',
+		                'rgba(255, 206, 86, 1)',
+		                'rgba(75, 192, 192, 1)',
+		                'rgba(153, 102, 255, 1)',
+		                'rgba(255, 159, 64, 1)'
+		            ],
+		            borderWidth: 1
+		        }]
+		    },
+		    options: {
+		    	responsive: false,
+		        scales: {
+		            yAxes: [{
+		                ticks: {
+		                    beginAtZero:true
+		                }
+		            }]
+		        }
+		    }
+		});
+
+		var second = document.getElementById("secondChart");
+		var myDoughnutChart = new Chart(second, {
+    		type: "doughnut",
+   			data: {
+   				labels: ["Flexible", "Moderate", "Strict", "Super Strict"],
+		        datasets: [{
+		            data: [671, 786, 1025, 7],
+		            backgroundColor: [
+		                "rgba(255, 99, 132, 1)",
+		                "rgba(54, 162, 235, 1)",
+		                "rgba(255, 206, 86, 1)",
+		                "rgba(75, 192, 192, 1)",
+		            ],
+		            borderColor: [
+		                "rgba(255,99,132,1)",
+		                "rgba(54, 162, 235, 1)",
+		                "rgba(255, 206, 86, 1)",
+		                "rgba(75, 192, 192, 1)",
+		            ],
+		            borderWidth: 1
+		        }]
+   			},
+    		options: {
+		    	responsive: false,
+		    }
+		});
+
+		var third = document.getElementById("thirdChart");
+		var thirdChart = new Chart(third, {
+		    type: 'bar',
+		    data: {
+		        labels: ["Other", "House", "Condominum", "Bed & Breakfast", "Boat", "Treehouse"],
+		        datasets: [{
+		            label: '# of Listings',
+		            data: [5002, 2489, 547, 92, 6, 3],
 		            backgroundColor: [
 		                'rgba(255, 99, 132, 0.2)',
 		                'rgba(54, 162, 235, 0.2)',
@@ -48,6 +119,7 @@
 		        }]
 		    },
 		    options: {
+		    	responsive: false,
 		        scales: {
 		            yAxes: [{
 		                ticks: {
